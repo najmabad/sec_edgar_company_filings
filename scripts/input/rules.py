e5b93a7e78c799b4215c25067068154d873dff1f
Najma Bader <najmabdr@gmail.com>
2019-01-31 15:11:41 -0600

    fixed minor bugs

---
+++ scripts/input/rules.py
@@ -0,0 +1,23 @@
+import re
+
+RULES = [
+    ('total_words', re.compile(r"\b\w+\b")),
+    ('variable_cost_ct', re.compile(r"\bvariable\b \bcosts?\b")),
+    ('variable_exp_ct', re.compile(r"\bvariable\b \bexpenses?\b")),
+    ('fixed_exp_ct', re.compile(r"\bfixed\b \bexpenses?\b")),
+    ('fixed_cost_ct', re.compile(r"\bfixed\b \bcosts?\b")),
+    ('large_fixed_ct', re.compile(r"\blarge\b \bfixed\b")),
+    ('sign_fixed_ct', re.compile(r"\bsignificant\b \bfixed\b")),
+    ('rel_fixed_ct', re.compile(r"\brelatively\b \bfixed\b")),
+    ('mostly_fixed_ct', re.compile(r"\bmostly\b \bfixed\b")),
+    ('just_in_time_ct', re.compile(r"\bjust\b \bin\b \btime\b")),
+    ('lean_ct', re.compile(r"\blean\b")),
+    ('tot_quality_mng_ct', re.compile(r"\btotal\b \bquality\b \bmanagement\b")),
+    ('six_sigm_ct', re.compile(r"\bsix\b \bsigma\b")),
+    ('scale_econ_ct', re.compile(r"\bscale\b \beconomies\b")),
+    ('scope_econ_ct', re.compile(r"\bscope\b \beconomies\b")),
+    ('econ_of_scale_ct', re.compile(r"\beconomies\b \bof\b \bscale\b")),
+    ('econ_of_scope_ct', re.compile(r"\beconomies\b \bof\b \bscope\b")),
+    ('unv_ct', re.compile(r"\buniversity\b \bof\b")),
+    ('outsource_ct', re.compile(r"\boutsource\b|\boutsourcing\b|\boutsourced\b")),
+]
