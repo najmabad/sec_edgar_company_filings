74a771c53014f45bda27168dabcfe80bbe6a9c1e
Najma Bader <najmabdr@gmail.com>
2019-02-07 18:48:13 -0600

    .

--- scripts/count_words.py
+++ scripts/count_words.py
@@ -18,6 +18,7 @@ def main(rules):
 
     csv_lines = [",".join(headers)]
     for file_name, counts, doc_type in tqdm(pool.imap_unordered(process_file, [(file, rules) for file in files]), total=len(files)):
+        print(counts)
         csv_lines.append(
             f"{file_name.replace('_', ',')},{doc_type},{','.join([str(counts[rule[0]+suffix]) for rule in rules for suffix in ['_clean', '_no_clean']])}"
         )
